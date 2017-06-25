178629d4422d92aa28fbad45c9a259ad05b381e2
Joseph Siribandan Sriprajittichai <jsripraj@ecegrid-thin1.ecn.purdue.edu>
2017-06-24 22:49:37 -0400

    shell insertion working

--- main.c
+++ main.c
@@ -11,16 +11,25 @@ int main(int argc, char **argv) {
 
 	// Load elements from file into an array
 	long * Array = Load_From_File(argv[2], &Size);
-	printf("\nArray:");
+	printf("\n\nArray In:");
 	for (int i = 0; i < Size; i++) {
 		printf("\n%ld", Array[i]);
 	}
-	
+
 	// Print out the sequence
-	Print_Seq(argv[3], Size);	
+	Print_Seq(argv[3], Size);
+
+	double comps = 0, moves = 0;
+
+	// Shell insertion sort
+	Shell_Insertion_Sort(Array, Size, &comps, &moves);
 
 	// Write the elements from array into file
 	int nels = Save_To_File(argv[4], Array, Size);
-	printf("\nSize = %d\n", nels);
+	printf("\n\nnels = %d", nels);
+	printf("\n\nArray Out:");
+	for (int i = 0; i < Size; i++) {
+		printf("\n%ld", Array[i]);
+	}
 	return 0;
 }
--- sorting.c
+++ sorting.c
@@ -27,24 +27,25 @@ int Save_To_File(char * Filename, long * Array, int Size) {
 }
 
 int * Get_Seq(int Size, int *Seq_Nels) {
-	int q = 0, row = 0, i = 0;
-	int t1 = 1, t2 = 1;
+	int q = 0, row = 0, i = 0; // q is from 3^q, row is "pyramid" row
+	int t1 = 1, t2 = 1; // t1 is first term (2^p), t2 is second term (3^q)
 	while(t1 * t2 < Size) {
-		if (q == row) {
+		if (q == row) { // Moving to next row, so reset t1 and t2
 			row++;
 			q = 0;
 			t2 = 1;
 			for (t1 = 1, i = 0; i < row; i++) { t1 *= 2; }
-		} else {
+		} else { // Moving along row, t1 and t2 change powers
 			q++;
 			t1 /= 2;
 			t2 *= 3;
 		}
-		(*Seq_Nels)++;
+		(*Seq_Nels)++; // Keep track of elements in sequence
 	}
-	int *seq = malloc((*Seq_Nels) * sizeof(int));	
+	int *seq = malloc((*Seq_Nels) * sizeof(int));
 	int index = 0;
 	i = 0; q = 0; row = 0; t1 = 1; t2 = 1;
+	// Same algorithm. But this time store the sequence in the array
 	while(t1 * t2 < Size) {
 		seq[index] = t1 * t2;
 		if (q == row) { 
@@ -62,11 +63,39 @@ int * Get_Seq(int Size, int *Seq_Nels) {
 	return seq;
 }
 
-/*
 void Shell_Insertion_Sort(long * Array, int Size, double * N_Comp, double * N_Move) {
-
+	int i, j, k, x, seq_nels = 0;
+	long temp;
+	// Generate sequence
+	int * seq = Get_Seq(Size, &seq_nels);
+	// For each element in sequence (going backwards towards 1)...
+	for (i = seq_nels - 1; i >= 0; i--) {
+		// For each "column" in shell sort...
+		for (j = 0; j < seq[i]; j++) {
+			// For each "insertion" in insertion sort...
+			for (k = j + seq[i]; k < Size; k += seq[i]) {
+				x = k;
+				do {
+					printf("\nx = %d, Array[0] = %ld", x, Array[0]);
+					(*N_Comp)++;
+					// Swap until correct position or break out
+					if (Array[x] < Array[x - seq[i]]) {
+						temp = Array[x];
+						Array[x] = Array[x - seq[i]];
+						Array[x - seq[i]] = temp;
+						(*N_Move) += 3;
+						x -= seq[i];
+					} else {
+						x = -1;
+					}
+				} while (x - seq[i] >= 0);
+			}
+		}
+	}
+	return;
 }
 
+/*
 void Shell_Insertion_Sort(long * Array, int Size, double * N_Comp, double * N_Move) {
 
 }
@@ -75,7 +104,9 @@ void Shell_Insertion_Sort(long * Array, int Size, double * N_Comp, double * N_Mo
 int Print_Seq(char * Filename, int Size) {
 	FILE * fp = fopen(Filename, "w");
 	int seq_nels = 0;
+	// Generate the sequence
 	int * seq = Get_Seq(Size, &seq_nels);
+	// Write to file
 	for (int i = 0; i < seq_nels; i++) {
 		fprintf(fp, "%d\n", seq[i]);
 	}
